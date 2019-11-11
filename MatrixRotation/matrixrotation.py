e09d27a3b651c9f991020adaf5cd09d8eee5622c
Ben Hunter <code@benhunter.me>
2019-11-10 23:39:11 -0800

    in progress

--- MatrixRotation/matrixrotation.py
+++ MatrixRotation/matrixrotation.py
@@ -7,16 +7,65 @@ import re
 import sys
 
 # Complete the matrixRotation function below.
-def matrixRotation(matrix, r):
+def matrixRotation(matrix, rotations):
     print(matrix)
-    print(r)
+    print(rotations)
+
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    print("rows", rows)
+    print("columns", columns)
+
+    # make rings
+    rings = []
+    rings.append([])
+
+    # how many rings?
+    num_rings = min(rows, columns) // 2
+
+    for current_ring in range(num_rings):
+        start_pos = {'row': current_ring, 'column': current_ring}
+        print(' start pos', start_pos)
+
+        down = rows - (current_ring * 2)
+        print(' down', down)
+        
+        right = columns - (current_ring * 2)
+        print(' right', right)
+
+        # down
+        print('   down')
+        for i in range(down):
+            # print(i)
+            print('  ', start_pos['row'] + i, start_pos['column'])
+        
+        # right
+        print('   right')
+        for i in range(right - 1):
+            print('  ', start_pos['row'] + down - 1, start_pos['column'] + i + 1)
+        
+        # up
+        print('   up')
+        for i in range(down - 1):
+            print('  ', start_pos['row'] + down - 2 - i, start_pos['column'] - 1 + right)
+
+        # left
+        print('   left')
+        for i in range(right - 2):
+            print('  ', start_pos['row'], start_pos['column'] - 2 + right - i)
+
+
+    # rotate each ring
+
+    # output
 
 if __name__ == '__main__':
     mnr = input().rstrip().split()
 
-    m = int(mnr[0])
+    m = int(mnr[0])  # rows
 
-    n = int(mnr[1])
+    n = int(mnr[1])  # columns
 
     r = int(mnr[2])
 
