c15af0cc9a62f891805ce5b0a3c50571a1c7a678
Ben Hunter <code@benhunter.me>
2019-11-11 23:17:41 -0800

    rotations complete, working output

--- MatrixRotation/matrixrotation.py
+++ MatrixRotation/matrixrotation.py
@@ -6,59 +6,93 @@ import random
 import re
 import sys
 
+from collections import deque
+from copy import deepcopy
+
+
+DEBUG = True
+
 # Complete the matrixRotation function below.
 def matrixRotation(matrix, rotations):
-    print(matrix)
-    print(rotations)
 
     rows = len(matrix)
     columns = len(matrix[0])
 
-    print("rows", rows)
-    print("columns", columns)
+    if DEBUG:
+        print(matrix)
+        print(rotations)
+        print("rows", rows)
+        print("columns", columns)
 
     # make rings
-    rings = []
-    rings.append([])
+    rings = deque([])
 
     # how many rings?
     num_rings = min(rows, columns) // 2
 
     for current_ring in range(num_rings):
+        if DEBUG: print('ring', current_ring)
+
+        rings.append(deque([]))
+
         start_pos = {'row': current_ring, 'column': current_ring}
-        print(' start pos', start_pos)
+        if DEBUG: print(' start pos', start_pos)
 
         down = rows - (current_ring * 2)
-        print(' down', down)
+        if DEBUG: print(' down', down)
         
         right = columns - (current_ring * 2)
-        print(' right', right)
+        if DEBUG: print(' right', right)
 
         # down
-        print('   down')
+        if DEBUG: print('   down')
         for i in range(down):
-            # print(i)
-            print('  ', start_pos['row'] + i, start_pos['column'])
+            current_row = start_pos['row'] + i
+            current_column = start_pos['column']
+            rings[current_ring].append(matrix[current_row][current_column])
+            if DEBUG: print('  ', current_row, current_column, matrix[current_row][current_column])
         
         # right
-        print('   right')
+        if DEBUG: print('   right')
         for i in range(right - 1):
-            print('  ', start_pos['row'] + down - 1, start_pos['column'] + i + 1)
+            current_row = start_pos['row'] + down - 1
+            current_column = start_pos['column'] + i + 1
+            rings[current_ring].append(matrix[current_row][current_column])
+            if DEBUG: print('  ', current_row, current_column, matrix[current_row][current_column])
         
         # up
-        print('   up')
+        if DEBUG: print('   up')
         for i in range(down - 1):
-            print('  ', start_pos['row'] + down - 2 - i, start_pos['column'] - 1 + right)
+            current_row = start_pos['row'] + down - 2 - i
+            current_column = start_pos['column'] - 1 + right
+            rings[current_ring].append(matrix[current_row][current_column])
+            if DEBUG: print('  ', current_row, current_column, matrix[current_row][current_column])
 
         # left
-        print('   left')
+        if DEBUG: print('   left')
         for i in range(right - 2):
-            print('  ', start_pos['row'], start_pos['column'] - 2 + right - i)
+            current_row = start_pos['row']
+            current_column = start_pos['column'] - 2 + right - i
+            rings[current_ring].append(matrix[current_row][current_column])
+            if DEBUG: print('  ', current_row, current_column, matrix[current_row][current_column])
 
+    if DEBUG: print(rings)
 
     # rotate each ring
+    for ring in rings:
+        rots = rotations % len(ring)
+        for i in range(rots):
+            ring.append(ring.popleft())
+    if DEBUG: print(rings)
 
     # output
+    rot_rings = deepcopy(rings)
+
+    for count, ring in enumerate(rings):
+        if DEBUG: print(count, ring)
+        
+
+
 
 if __name__ == '__main__':
     mnr = input().rstrip().split()
