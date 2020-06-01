c66e704d3fbc4e815e6f49f3a7f26fc25d14b0b4
Ben Hunter <code@benhunter.me>
2020-05-31 17:30:16 -0700

    Started
    Signed-off-by: Ben Hunter <code@benhunter.me>

---
+++ InterviewPreparationKit/WarmUp/SockMerchant/sock.py
@@ -0,0 +1,17 @@
+# 2019-12-25
+#
+# https://www.hackerrank.com/challenges/sock-merchant/problem
+# 
+
+def main():
+    n = input()
+    socks = list(map(int, input().split()))
+    print(socks)
+
+
+def test_main():
+    main()
+
+
+if __name__ == "__main__":
+    main()
